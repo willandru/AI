05ea5d6d0d764f66e017ab13619a787a7d327420
willandru <willandru@hotmail.com>
2022-02-26 21:19:33 -0500

    It is displaying the enemy

--- ParchisIA/parchis.py
+++ ParchisIA/parchis.py
@@ -23,7 +23,7 @@ fichas_a_mover_P1= np.array([(0,0),(1,0),(2,0),(3,0)])
 fichas_a_mover_IA= np.array([(0,0),(1,0),(2,0),(3,0)])
 
 torres=0
-veces=40
+veces=20
 #fichas_posicion_P1[2]=5
 
 def lanzar():
@@ -45,8 +45,15 @@ def heuristicaP1():
 #heuristicaP1()
 
 def buscarEnemy(N):
+	print('BUSCANDO ENEMY......')
 	for i in range(0,4):
 		if(fichas_posicion_IA[i]==N):
+			return i
+
+def buscarEnemyAI(N):
+	print('BUSCANDO ENEMY......')
+	for i in range(0,4):
+		if(fichas_posicion_P1[i]==N):
 			return i
 
 def prepararCaminoP1(N,j):
@@ -125,7 +132,111 @@ def moverP1(N):
 
 
 
+def heuristicaAI():
+	contador=0
+	for x in range(0,4):
+		fin=  heuristicaP1() ##LA FICHA P1
+		inicio=fichas_posicion_IA[x]   
+		distancia= fin-inicio ## PUEDEN SALIR NUMEROS NEGATIVOS, pues ya esta adelante de la ficha¡
+		fichas_a_mover_IA[contador][1]=distancia
+		contador+=1
+	#print(fichas_a_mover_P1)
+	h= fichas_a_mover_IA[:,1].argsort()
+	#print(h)
+	return h
+	
 
+def prepararCaminoAI(N,j):
+	barrera=False
+	posicion_inicial=j
+	print(N)
+	print(j)
+	print('PRAPARANDO EL CAMINO. en  AIIIII**')		
+	for i in range(1,1+N):
+		print(posicion_inicial)
+		print(i)
+		print(posicion_inicial+i)
+		#print(tablero_fichas[int(posicion_inicial+i)])
+		if(int(posicion_inicial+i)>67):
+			break
+		elif( tablero_fichas[int(posicion_inicial)]==1 and tablero_tipo[int(posicion_inicial+i)]==2 and tablero_seguros[int(posicion_inicial+i)]!=1):
+			enemy_posicion=int(posicion_inicial+i)
+			uid=buscarEnemy(enemy_posicion) #ID de la ficha del enemigo
+			fichas_posicion_IA[uid]= -1
+			tablero_tipo[enemy_posicion]=0
+			print('RIVAL COMIDO******')						 #ACA SE PONE LA FICHA DEL ENEMIGO EN LA CARCEL , SE UBICA LA NUESTRA
+		elif(tablero_fichas[int(posicion_inicial+i)]==2):
+			barrera_posicion=int(posicion_inicial+i)
+			barrera=True
+			torres=int(posicion_inicial+i)
+			print('BARRERA ENCONTRADA******')		
+	return barrera
+
+def comerseP1(k):
+	for x in range(0,4):
+		if fichas_posicion_P1[x]==k:
+			fichas_posicion_P1[x]=-1
+
+		
+	
+
+def moverAI(N):
+	h=heuristicaAI()
+	z=h[0] #ID
+	w=h[1]
+	
+	posicion_inicial=int(fichas_posicion_IA[z]) #Posisicion inicial del movimiento de la ficha
+	barrera=False
+
+	if (posicion_inicial+N)> 67:
+		n=int(posicion_inicial+N)-67
+		fichas_posicion_IA[z]=0
+		tablero_fichas[posicion_inicial]-=1
+		tablero_tipo[posicion_inicial]=0
+		posicion_inicial=0
+		N=n
+
+	if posicion_inicial!=-1 :
+		barrera=prepararCaminoAI(N,posicion_inicial) #Se mandan a la carcel y se buscan barreras
+
+	if barrera:
+		moverA= torres - posicion_inicial
+		moverB= (posicion_inicial+N)- torres
+		
+		tablero_fichas[int(fichas_posicion_IA[z])]-=1
+		tablero_fichas[int(fichas_posicion_IA[w])]-=1
+
+		tablero_tipo[int(fichas_posicion_IA[z])]=0
+		tablero_tipo[int(fichas_posicion_IA[w])]=0
+
+		fichas_posicion_IA[z]+=moverA
+		fichas_posicion_IA[w]+=moverB
+
+		tablero_tipo[int(fichas_posicion_IA[z])]=1
+		tablero_tipo[int(fichas_posicion_IA[w])]=1
+	else:
+		print('Moviendo a la AI***********:')
+		print(N)
+		h=heuristicaAI()
+		z=h[0]
+		if int(fichas_posicion_IA[z]) < -20:
+			pass
+		else:
+			k=int(fichas_posicion_IA[z])+N
+			if tablero_tipo[k]==1:
+				comerseP1(k)
+				tablero_fichas[int(fichas_posicion_IA[z])]-=1
+				tablero_fichas[int(fichas_posicion_IA[z])+N]+=1
+				tablero_tipo[int(fichas_posicion_IA[z])]=0
+				fichas_posicion_IA[z]+=N
+				tablero_tipo[int(fichas_posicion_IA[z])]=2
+			else:
+				tablero_fichas[int(fichas_posicion_IA[z])]-=1
+				tablero_fichas[int(fichas_posicion_IA[z])+N]+=1
+				tablero_tipo[int(fichas_posicion_IA[z])]=0
+				fichas_posicion_IA[z]+=N
+				tablero_tipo[int(fichas_posicion_IA[z])]=2
+			
 
 
 
@@ -159,8 +270,8 @@ while(not ganador):
         print(tablero_fichas)
         print(fichas_posicion_P1)
         print(int(np.sum(fichas_coronadas_P1)))
-        if(int(np.sum(fichas_coronadas_P1))==4):
-        	print('NOt ganador')
+        if(int(np.sum(fichas_coronadas_P1) or np.sum(fichas_coronadas_IA))==4):
+        	print('Not ganador')
         	ganador=True
         	break
         print('Juega Player')
