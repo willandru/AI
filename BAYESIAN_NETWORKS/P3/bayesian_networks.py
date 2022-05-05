144833dd3a104cd58a5aa873f2f701801d7061b0
willandru <willandru@hotmail.com>
2022-05-04 19:31:08 -0500

    Dictionary created

--- BAYESIAN_NETWORKS/P3/bayesian_networks.py
+++ BAYESIAN_NETWORKS/P3/bayesian_networks.py
@@ -51,13 +51,13 @@ print('values: ', tls)
 
 #SOLICITAR LAS PROPBAILIDADES :
 
+word_dict={}
+
 i=0
 for tabla in nodes:
 	print('-'*90)
 
 	print('TABLA DE PROBABILIDAD PARA :' , tabla)
-	print('COn Dependencias: ', deps[i], i)
-
 	for probabilidades in tls[i]:
 		#print(probabilidades)
 		txt_0=str(probabilidades)
@@ -66,12 +66,12 @@ for tabla in nodes:
 		j=0
 		to_find=[]
 		for D in deps[i]:
-			print('QUE_EJESO', D)
 			if D=='' and len(deps[i])== 1:
 				txt_2+=' and'+str(D)+'='
 				ult='P('+txt_1+'='+txt_0+')'
 				ult=ult.replace(' ','')
-				print(ult)
+				p00=input(ult+':')
+				word_dict.update({ult:p00 })
 				
 			elif D !='' and len(deps[i])== 1:
 				txt_2+=str(D)+'='
@@ -86,13 +86,12 @@ for tabla in nodes:
 						for F in tls[u]:
 							fin='P('+txt_1+'='+txt_0+'|'+given+''+F+')'
 							fin=fin.replace(' ','')
-							print(fin)
-							input()
+							p01=input(fin+':')
+							word_dict.update({fin:p01})
 					u+=1
 
 
 			else:
-				print('Mundo')
 				if j==0:
 					txt_2+=str(D)+'='
 					to_find.append(str(D))
@@ -105,10 +104,7 @@ for tabla in nodes:
 			j+=1
 		v=[]
 		if len(deps[i])>1:
-			print('WELCOME: ')
-			print(to_find)
 						
-			
 			for word in to_find:
 				u=0
 				for buscar in nodes:
@@ -120,13 +116,12 @@ for tabla in nodes:
 					u+=1
 				v.append(pos)
 
-			print('Same: ', v)
+			
 			combine_lists=[]
 			for pos in v:
 				combine_lists.append(tls[pos])
 			alfa=len(combine_lists)
-			print('Combined LIst: ', combine_lists)
-			print('alfa: ', alfa)
+			
 			
 			#Make vector BETA
 			beta=[]
@@ -137,18 +132,16 @@ for tabla in nodes:
 				size_probas=size_probas*n
 				l=list(range(n))
 				beta.append(l)
-			print(beta)
-
+			
 			#Make Probas:
 			probas=[]
 
-			print('Este? ',size_probas)
-
+			
 			for z in beta[0]:
 				for j in beta[1]:
 					x=[z,j]
 					probas.append(x)
-			print('*_*[**',probas)
+			
 
 			if len(deps[i])==2:
 				for X in probas:
@@ -162,8 +155,8 @@ for tabla in nodes:
 					final_txt=t_1+word_1+','+t_2+word_2
 					final='P('+txt_1+'='+txt_0+'|'+final_txt+')'
 					final=final.replace(' ','')
-					print(final)
-					input('Got u: ')
+					p02=input(final+':')
+					word_dict.update({final:p02})
 			
 
 			# Make the combinations from 2-n
@@ -174,74 +167,43 @@ for tabla in nodes:
 			continua=True
 
 			for x in range(alfa):
-				print('X IN ALFAAA... : ', x)
-				print('P is: ', P)
-
+				
 				if P:
 					probas=P
 					P=[]
 				
 				if x>1:
-					print('betax ',beta[x])
 					m=len(beta[x])
-					print('M: ', m)
-					
-					print('PORBAS:', probas)
-					print('MI VECTOR: ', P)
 					for value in beta[x]:
-						print('que value es:', value)
-						
 						for pp in probas:
 							k=[]
-							print('k:', k)
-							print('esto es pp:', pp)
-							print(pp, value)
 							for y in pp:
-								print(y)
 								k.append(y)
 							k.append(value)
-							print('k:', k)
-							print(k)
 							P.append(k)
-
-
-
-
-					
-							
-							
-			print(P)
-			print('total len ', len(P))
 
 			if len(P)== size_probas:
 				for p in P:
-					print(p)
-					print('PpppP: ', v, to_find)
 					traslate=[]
 					TAILS=[]
 					for h in v:
 						TAILS.append(tls[h])
-					print(TAILS)
-
 					k=[]
 					o=0
 					for pos in p:
 						k.append(TAILS[o][pos])
 						o+=1
-
-					print(k, '<------')
-
 					len_ans=len(k)
 					TXT=''
 					for w in range(len_ans):
 						TEMP=str(to_find[w])+'='+str(k[w])
 						TXT+=',' +TEMP
-						print('S,e,,,,', TXT[1:]) 
 					TXT=TXT[1:]
 
 					fT='P('+txt_1+'='+txt_0+'|'+TXT+')'
 					ff=fT.replace(' ','')
-					x=input(ff)
+					p03=input(ff+':')
+					word_dict.update({ff:p03})
 						
 
 
@@ -269,3 +231,6 @@ for tabla in nodes:
 
 
 	i+=1
+
+
+print(word_dict)
